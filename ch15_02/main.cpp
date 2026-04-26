f7ce45a153babf4a2eacd5142c4c90ba595a04de
brandontieu626 <brandontieu626@gmail.com>
2026-04-26 16:56:39 -0500

    Chapter 15.5 - Class templates with member functions

--- ch15_02/main.cpp
+++ ch15_02/main.cpp
@@ -1,4 +1,19 @@
 #include <iostream>
+// Question #1
+// What is the purpose of defining member functions outside the class definition?
+// d) All of the above
+
+// How do you define a member function outside the class definition?
+// b) Define the function with the class name prefixed using the scope resolution operator (::).
+
+// When should trivial member functions be defined inside the class definition ?
+// b) When the functions have a single line of code.
+
+// Where should the class definition be placed to facilitate reuse in multiple files or projects?
+// b) In a separate header file with the same name as the class.
+
+// Which of the following is true about the one-definition rule for classes and member functions?
+// c) Member functions defined inside the class definition are exempt from the one-definition rule.
 
 int main()
 {
---
+++ ch15_05/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+// Question #1
+// Write a class template named Triad that has 3 private data members 
+// with independent type template parameters. 
+// The class should have a constructor, access functions, 
+// and a print() member function that is defined outside the class.
+
+template <typename T, typename U, typename Z>
+class Triad
+{
+public:
+    Triad(const T& first, const U& second, const Z& third)
+        : m_first{ first },
+        m_second{ second },
+        m_third{ third }
+    {
+    };
+
+    constexpr const T& first() const { return m_first; };
+    constexpr const U& second() const { return m_second; };
+    constexpr const Z& third() const { return m_third; };
+    
+    void print() const;
+
+private:
+    T m_first{};
+    U m_second{};
+    Z m_third{};
+};
+
+template <typename T, typename U, typename Z>
+void Triad<T,U,Z>::print() const {
+    std::cout << "[" << m_first << ", " << m_second << ", " << m_third << "]\n";
+}
+int main()
+{
+    Triad<int, int, int> t1{ 1, 2, 3 };
+    t1.print();
+    std::cout << '\n';
+    std::cout << t1.first() << '\n';
+
+    using namespace std::literals::string_literals;
+    const Triad t2{ 1, 2.3, "Hello"s };
+    t2.print();
+    std::cout << '\n';
+
+    return 0;
+}
+
